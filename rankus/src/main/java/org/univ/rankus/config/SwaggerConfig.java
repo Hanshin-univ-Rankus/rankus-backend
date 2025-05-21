4cec61adc801bc85a531bd4340048c4d15afcd5e
NohSungBeen <castlekong1019@gmail.com>
2025-05-21 14:39:18 +0900

    feat: SwaggerConfig 추가
    - SwaggerConfig 추가

---
+++ rankus/src/main/java/org/univ/rankus/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package org.univ.rankus.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI rankusOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Rankus Lab Management API")
+                        .version("v1.0.0")
+                        .description("교내 랩실 홍보·운영을 위한 REST API 문서"));
+    }
+}
