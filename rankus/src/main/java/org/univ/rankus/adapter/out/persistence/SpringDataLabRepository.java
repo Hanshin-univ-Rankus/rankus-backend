1b5a1e90e43a43b45ff99d20f8377e34c049c691
NohSungBeen <castlekong1019@gmail.com>
2025-05-21 14:46:54 +0900

    feat: SpringDataLabRepository 추가
    - SpringDataLabRepository 추가

---
+++ rankus/src/main/java/org/univ/rankus/adapter/out/persistence/SpringDataLabRepository.java
@@ -0,0 +1,11 @@
+package org.univ.rankus.adapter.out.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.univ.rankus.domain.model.Lab;
+
+/**
+ * Lab Entity에 대한 CRUD를 제공하는 Spring Data JPA 리포지토리
+ */
+public interface SpringDataLabRepository extends JpaRepository<Lab, Long> {
+    // 추가 커스텀 쿼리 메서드는 여기에 선언
+}
